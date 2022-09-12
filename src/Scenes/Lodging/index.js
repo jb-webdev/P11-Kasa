685e7638fe025ab374c7881d9a0b4f1719307200
jb.webdev <j.boero@icloud.com>
2022-09-12 10:26:42 +0200

    MEP condition pour gerer un id inexistant dans la BDD redirection vers la page error

--- src/Scenes/Lodging/index.js
+++ src/Scenes/Lodging/index.js
@@ -4,19 +4,32 @@
  * Date : Aout 2022
  */
 import React from 'react';
+import { useParams } from 'react-router-dom'
 import './style.css'
 
 import GetLodging from './GetLodging.js'
 import Error from '../Error/index.js'
-// recupere l'id useParams
-// retour page error
-let isOK = true
+
+import datas from '../../services/data/datas.json'
+
 
 export default function Lodging() {
+  // On utilise UseParmas pour recuperer l'id
+  const params = useParams()
+  // On utilise un function pour verifier si l'id existe ou pas
+  const verifyId= (dataToVerify) => {
+    var ifExist = false
+    for (let i =0; i < dataToVerify.length; i++){
+      if (dataToVerify[i].id.includes(params.id)) {
+        return ifExist = true
+      }
+    }
+    return ifExist
+  }
+
   return (
     <main className="lodgingMain">
-      {isOK ? <GetLodging /> : <Error />}
-      
+      {verifyId(datas) ? <GetLodging /> : <Error />}
     </main>
   )
 }
